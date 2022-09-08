fdbf9c5fe4fb2c8cb6915dae5d22f7ee26df763b
Adel <gerencser.adel@gmail.com>
2022-09-08 14:24:42 +0200

    render results

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ let form = document.querySelector(".js-form");
 let inputField = document.querySelector("[name=word]");
 let results = document.querySelector(".js-results");
 
-function renderResults(results) {
-    console.log(results)
+function renderMeanings() {
+    return "def"
+}
+
+function renderDatas(datas) {
+    let html = `<h2>${datas.word} ${datas.phonetic}</h2>`;
+    renderMeanings(datas.meanings);
+    return html;
+}
+
+function renderResults(datas) {
+    let html = "";
+    if (Array.isArray(datas) && datas.length > 0) {
+        html = renderDatas(datas[0])
+    } else {
+        html = `<div class="error">The requested word does not exits.</div>`
+    }
+    return results.innerHTML = html;
 }
 
 function getResults(event) {
